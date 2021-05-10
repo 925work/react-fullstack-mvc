a1c4abc47e09cbd8c06f262e006d661ab04596ac
Alex Pham <925workk@gmail.com>
2021-05-10 17:32:58 -0600

    Added ability to add comments as well as user authentication.

--- server/index.js
+++ server/index.js
@@ -10,9 +10,13 @@ const db = require("./models");
 // Routers
 const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
+const commentsRouter = require("./routes/Comments");
+app.use("/comments", commentsRouter);
+const usersRouters = require("./routes/Users");
+app.use("/auth", usersRouters);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
-    console.log("Server running on port 3001!");
+    console.log("Server running on port 3001");
   });
 });
